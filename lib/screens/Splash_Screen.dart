72130c2c4f61e246c95067ebda905573c9edfa6c
Avinash <avinash2596360@gmail.com>
2025-10-19 23:52:28 +0530

    New Update Made-1

--- lib/screens/Splash_Screen.dart
+++ lib/screens/Splash_Screen.dart
@@ -1,26 +1,9 @@
-import 'package:flutter/material.dart';
 
-void main() {
-runApp(EduProApp());
-}
-
-class EduProApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'EDUPRO',
-debugShowCheckedModeBanner: false,
-theme: ThemeData(
-primaryColor: Color(0xFF6A5AE0),
-scaffoldBackgroundColor: Colors.white,
-fontFamily: 'Inter',
-),
-home: SplashScreen(),
-);
-}
-}
 
 // ========== SCREEN 1: SPLASH SCREEN ==========
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class SplashScreen extends StatefulWidget {
 @override
 _SplashScreenState createState() => _SplashScreenState();
