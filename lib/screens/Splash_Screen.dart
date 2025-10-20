ef3900d0f95bf0c5b49f106f8251dc14c17a6a16
Avinash <avinash2596360@gmail.com>
2025-10-20 16:10:33 +0530

    Updated Splash Screen

--- lib/screens/Splash_Screen.dart
+++ lib/screens/Splash_Screen.dart
@@ -1,4 +1,25 @@
-
+// import 'package:edupro/screens/Splash_Screen.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(EduProApp());
+// }
+//
+// class EduProApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'EDUPRO',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primaryColor: Color(0xFF6A5AE0),
+//         scaffoldBackgroundColor: Colors.white,
+//         fontFamily: 'Inter',
+//       ),
+//       home: SplashScreen(),
+//     );
+//   }
+// }
 
 // ========== SCREEN 1: SPLASH SCREEN ==========
 import 'package:flutter/cupertino.dart';
